5e0bfb724187bb5b25b08f11e2cf0823373625fd
Milan Felix Šulc <rkfelix@gmail.com>
2018-11-11 14:24:34 +0100

    Layout: use bulma instead of bootstrap

--- assets/main.ts
+++ assets/main.ts
@@ -1,17 +1,19 @@
 // Bootstrap 4 (CSS + JS)
-import 'bootstrap/js/src/tab';
-import './main/css/bootstrap.scss';
+//import 'bootstrap/js/src/tab';
+//import './main/css/bootstrap.scss';
 
 // Font-awesome 4 (FTW)
 import 'font-awesome/scss/font-awesome.scss';
 
 // Highlight.js
-import './main/js/hljs';
+//import './main/js/hljs';
 
 // CSS theme
-import "./main/css/theme.less";
+//import "./main/css/theme.less";
 
 // Vue apps
-if (document.getElementById('app-tools')) {
-    import('./main/vue/app-tools');
-}
+//if (document.getElementById('app-tools')) {
+//    import('./main/vue/app-tools');
+//}
+
+import 'bulma/css/bulma.css'
